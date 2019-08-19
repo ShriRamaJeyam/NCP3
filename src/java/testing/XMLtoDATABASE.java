3733e0edfd136e7208ffca351cba9e4c05059148
Niranjan Kumar K S <niranjankumar26081998@gmail.com>
2019-08-19 20:38:50 +0530

    dlkjdkl

---
+++ src/java/testing/XMLtoDATABASE.java
@@ -0,0 +1,21 @@
+package testing;
+import java.sql.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class XMLtoDATABASE 
+{
+    public static void main(String args[])
+    {
+        Connection con;
+        try
+        {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); 
+        }
+        catch(Exception e)
+        {
+            ;
+        }
+        
+    }
+}
